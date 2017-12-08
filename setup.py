8b9774bb29daea0cda40413d833480ba7826360e
Ruben Nørgaard <rubennorgaard@Rubens-MacBook-Pro.local>
2017-12-08 15:10:10 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'django-settings-cmd',
+  packages = ['django-settings-cmd'], 
+  version = '0.1',
+  description = 'A commmand line tool to manage the django settings file',
+  author = 'Ruben Nørgaard',
+  author_email = 'email@rubennoergaard.dk',
+  url = 'https://github.com/RubenNorgaard/django-settings-cmd', # use the URL to the github repo
+  download_url = 'https://github.com/RubenNorgaard/django-settings-cmd/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['django', 'settings', 'commandline'], # arbitrary keywords
+  classifiers = [],
+)
